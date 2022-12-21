554cbd0f8749ade12aa8c6007cc4c61df56046f8
ad56583964 <1176949486@qq.com>
2022-12-22 03:39:58 +0800

    :memo: update README

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-
-
-
 
 function App() {
-
-    const [pastedText,setpastedText] = useState(null)
-    function handlePaste(e){
-        const cacheHtml = e.clipboardData.getData('text/html')
-        console.log(cacheHtml)
-        setpastedText(cacheHtml)
-    }
-
   return (
     <div className="App">
-        <textarea onPaste={handlePaste} value={pastedText}></textarea>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
